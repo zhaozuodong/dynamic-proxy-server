ba708b45a38750a6ede64dcca3732c3e40dda27d
zhaozuodong <tenjor@163.com>
2019-07-17 15:28:48 +0800

    更新

--- src/main/java/pub/zzd/main/ProxyMain.java
+++ src/main/java/pub/zzd/main/ProxyMain.java
@@ -1,22 +1,21 @@
 package pub.zzd.main;
 
-import java.io.IOException;
+import pub.zzd.task.ProxyThread;
+
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ProxyMain {
     static Integer prot = 8988;
-
+    static Boolean isProxy = false;
     public static void main(String[] args) {
         try {
             // 在端口上创建一个Socket服务
             ServerSocket serverSocket = new ServerSocket(prot);
             while (true){
-                Socket localSocket = serverSocket.accept();
+                new ProxyThread(serverSocket.accept(),isProxy).start();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/main/java/pub/zzd/task/ProxyThread.java
+++ src/main/java/pub/zzd/task/ProxyThread.java
@@ -1,23 +1,34 @@
 package pub.zzd.task;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
 public class ProxyThread extends Thread {
-    Socket socket;
+    public Socket socket;
+    public Boolean isProxy;
 
-    public ProxyThread(Socket socket) {
+    public ProxyThread(Socket socket,Boolean isProxy) {
         this.socket = socket;
+        this.isProxy = isProxy;
     }
 
     @Override
     public void run() {
         try {
             InputStream inputStream = socket.getInputStream();
+            // 1、先获取请求内容
 
+            if (isProxy){
+                // 走第三方代理
+                Socket socket = new Socket("", 123);
 
-        } catch (IOException e) {
+            }else {
+                // 使用本地代理
+            }
+
+            // 完成后关闭资源
+            socket.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         super.run();
